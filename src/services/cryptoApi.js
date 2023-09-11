31606be03d334b15d4dd64d4e957d0874a88364f
abhishek1901ee04 <54908876+abhishek1901ee04@users.noreply.github.com>
2023-09-11 15:06:22 +0530

    pushing latest changes

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,28 @@
+import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '458c747553msh0966e44d7f6bbc3p1c1d17jsn97e8e447c6bf',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url,headers:cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath :"cryptoApi",
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints : (builder) =>({
+        getCryptos : builder.query({
+            query : (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails : builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery
+} = cryptoApi;
